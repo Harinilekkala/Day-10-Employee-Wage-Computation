490cbaf02d461f88a843eac008527bee6b485f49
Harinilekkala <harini.lekkala1997@gmail.com>
2022-05-24 14:13:57 +0530

    [Harini]: check attendance of the employee

---
+++ EmployeeWagecmp/EmployeeWagecmp/Program.cs
@@ -0,0 +1,16 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using System;
+
+namespace Employee
+{
+    internal class Employee
+    {
+        
+        public static void Main(String[] args)
+        {
+            EmployeeWagecmp.empwage getMethod = new EmployeeWagecmp.empwage();
+            getMethod.Attendance();
+        }
+    }
+}
---
+++ EmployeeWagecmp/EmployeeWagecmp/empwage.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmployeeWagecmp
+{
+    internal class empwage
+    {
+    public void Attendance()
+        {
+            Random value = new Random();
+            int randomCheck = value.Next(2);
+
+            switch (randomCheck)
+            {
+                case 0:
+                    {
+                        Console.WriteLine("Employee is Absent");
+                        break;
+                    }
+                case 1:
+                    {
+                        Console.WriteLine("Employee is Present");
+                        break;
+                    }
+            }
+        }
+    }
+}
